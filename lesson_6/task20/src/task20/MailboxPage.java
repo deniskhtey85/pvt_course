1b5bd5a3c5438a386b97ddd02d5ab841f8311f6e
Denis Khtey <khtej.denis@gmail.com>
2019-03-05 15:32:26 +0300

    Add task 20

---
+++ lesson_6/task20/src/task20/MailboxPage.java
@@ -0,0 +1,151 @@
+﻿package task20;
+
+import java.util.List;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class MailboxPage {
+
+	WebDriver driver;
+
+	@FindBy(xpath = "(//a[@data-name='link' and not(ancestor::div[contains(@style, 'display: none;')])])")
+	private List<WebElement> lettersList;
+
+	@FindBy(xpath = "(//div[contains(@class,'checkbox__box') and ancestor::div[@id='b-letters']and not(ancestor::div[contains(@style, 'display: none;')])])")
+	public List<WebElement> checkboxList;
+
+	@FindBy(xpath = "(//div[contains(@class, 'b-checkbox__box')])[1]")
+	public WebElement checkAll;
+
+	@FindBy(xpath = "(//div[@data-name='spam'])[1]")
+	public WebElement spamButton;
+
+	@FindBy(xpath = "(//div[@data-name='noSpam'])[1]")
+	public WebElement noSpamButton;
+
+	@FindBy(xpath = "//span[contains(text(),'Входящие')]")
+	public WebElement incomingMailFolder;
+
+	@FindBy(xpath = "//div[@id='b-nav_folders']//span[text()='Спам']")
+	public WebElement spamFolder;
+
+	@FindBy(xpath = "(//a[@data-name='compose'])[1]")
+	private WebElement writeLetterButton;
+
+	@FindBy(xpath = "(.//*[@data-original-name='To'])[1]")
+	private WebElement letterRecipient;
+
+	@FindBy(xpath = ".//*[@name='Subject']")
+	private WebElement topicLetter;
+
+	@FindBy(xpath = "//iframe")
+	public WebElement iframeTextMessage;
+
+	@FindBy(xpath = "(//div[@data-name='send'])[1]")
+	public WebElement sendButton;
+
+	@FindBy(xpath = ".//div[@class='message-sent__title']")
+	private WebElement messageSentTitle;
+
+	@FindBy(xpath = "//a[@data-name='link' and not(ancestor::div[contains(@style, 'display: none;')])]//div[contains(@class, 'b-flag_yes')]//b")
+	public List<WebElement> flagsList;
+
+	@FindBy(xpath = "(//div[contains(@class, 'b-dropdown_more')])[1]")
+	public WebElement moreButtons;
+
+	@FindBy(xpath = "(//a[@data-name='flag_no'])[1]")
+	public WebElement removeFlags;
+
+	public MailboxPage(WebDriver webDriver) {
+		PageFactory.initElements(webDriver, this);
+		driver = webDriver;
+	}
+
+	public void clickCheckbox(int index) {
+		try {
+			checkboxList.get(index).click();
+		} catch (IndexOutOfBoundsException ex) {
+			System.out.println(ex.getMessage());
+		}
+	}
+
+	public void clickSpamButton() {
+		spamButton.click();
+	}
+
+	public void clickNoSpamButton() {
+		noSpamButton.click();
+	}
+
+	public void goToSpamFolder() {
+		spamFolder.click();
+		driver.navigate().refresh();
+	}
+
+	public void moveLetterToSpam(int index) {
+		clickCheckbox(index);
+		clickSpamButton();
+	}
+
+	public void extractLetterFromSpam(int index) {
+		goToSpamFolder();
+		clickCheckbox(index);
+		clickNoSpamButton();
+		driver.navigate().refresh(); //
+	}
+
+	public void goToIncomingFolder() {
+		incomingMailFolder.click();
+		driver.navigate().refresh();
+	}
+
+	public void sentLetter(String receivers, String topic, String text) {
+		writeLetterButton.click();
+		letterRecipient.sendKeys(receivers);
+		topicLetter.sendKeys(topic);
+		enterMessage(text);
+		sendButton.click();
+	}
+
+	public void enterMessage(String message) {
+		driver.switchTo().frame(iframeTextMessage).findElement(By.xpath("//body")).sendKeys(message);
+		driver.switchTo().defaultContent();
+	}
+
+	public String getMessageSentTitle() {
+		return messageSentTitle.getText();
+	}
+
+	public List<WebElement> getLettersList() {
+		return lettersList;
+	}
+
+	public void markFlagOfFirst(int count) {
+		List<WebElement> messages = lettersList;
+		for (int row = 0; row < count; row++) {
+			(messages.get(row)).findElement(By.xpath(".//div[@data-act='flag']")).click();
+		}
+	}
+
+	public void removeAllFlag() {
+		checkAll.click();
+		moreButtons.click();
+		removeFlags.click();
+	}
+
+	public List<WebElement> getMarkTheFlag() {
+		return flagsList;
+	}
+
+	public void threadSleep() {
+		try {
+			Thread.sleep(3000);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+	}
+
+}
---
+++ lesson_6/task20/test/task20/MailboxPageTest.java
@@ -0,0 +1,91 @@
+﻿package task20;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+
+import java.util.concurrent.TimeUnit;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.FixMethodOrder;
+import org.junit.Test;
+import org.junit.runners.MethodSorters;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
+public class MailboxPageTest {
+
+	private WebDriver driver;
+	private MailboxPage mailboxPage;
+	private LoginPage loginPage;
+	private final String EMAIL = "d.k.h.t.e.y";
+	private final String PASSWORD = "Dm-1930013";
+	private final int TIMEOUT_FOR_IMPLICITLY = 10;
+	private final int TIMEOUT_FOR_LOAD = 10;
+
+	@Before
+	public void setUp() throws Exception {
+		driver = new ChromeDriver();
+		driver.manage().timeouts().implicitlyWait(TIMEOUT_FOR_IMPLICITLY, TimeUnit.SECONDS);
+		driver.manage().timeouts().pageLoadTimeout(TIMEOUT_FOR_LOAD, TimeUnit.SECONDS);
+		driver.manage().window().maximize();
+		driver.get("https://mail.ru");
+		loginPage = new LoginPage(driver);
+		mailboxPage = new MailboxPage(driver);
+		loginPage.enterDataForLogin(EMAIL, PASSWORD);
+		loginPage.clickEnterButton();
+		mailboxPage.goToIncomingFolder();
+	}
+
+	@Test // 1 перемещение письма в Спам
+	public void test1_moveLatterToSpamTest() {
+		String titleLetter = mailboxPage.getLettersList().get(0).getAttribute("data-subject");
+		mailboxPage.moveLetterToSpam(0);
+		mailboxPage.threadSleep();
+		mailboxPage.goToSpamFolder();
+		String titleLetterSpam = mailboxPage.getLettersList().get(mailboxPage.getLettersList().size() - 1)
+				.getAttribute("data-subject");
+		assertEquals(titleLetter, titleLetterSpam);
+	}
+
+	@Test // 2
+	public void test2_extractFromSpamTest() {
+		mailboxPage.moveLetterToSpam(0);
+		mailboxPage.goToSpamFolder();
+		String titleLetter = mailboxPage.getLettersList().get(0).getAttribute("data-subject");
+		mailboxPage.extractLetterFromSpam(0);
+		mailboxPage.threadSleep();
+		mailboxPage.goToIncomingFolder();
+		assertEquals(mailboxPage.getLettersList().get(0).getAttribute("data-subject"), titleLetter);
+	}
+
+	@Test // 5
+	public void test5_sendMessToGroupTest() {
+		String recipients = "khtej.denis+1@gmail.com, khtej.denis+2@gmail.com";
+		String topic = "TestTopic";
+		String text = "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor.";
+		mailboxPage.sentLetter(recipients, topic, text);
+		String expectedMess = "Ваше письмо отправлено. Перейти во Входящие";
+		assertEquals(mailboxPage.getMessageSentTitle(), expectedMess);
+	}
+
+	@Test // 3
+	public void test3_mark3LettersFlagTest() {
+		int numberMarked = 3;
+		mailboxPage.markFlagOfFirst(numberMarked);
+		assertEquals(numberMarked, mailboxPage.flagsList.size());
+	}
+
+	@Test // 4
+	public void test4_deselectAllFlagsTest() {
+		mailboxPage.removeAllFlag();
+		assertFalse(mailboxPage.getMarkTheFlag().size() > 0);
+	}
+
+	@After
+	public void tearDown() throws Exception {
+		driver.quit();
+	}
+
+}
