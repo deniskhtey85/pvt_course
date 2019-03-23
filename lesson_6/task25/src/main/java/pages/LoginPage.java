ab2be277eca8e390667952e3bb29d629a837b7a3
Denis Khtey <khtej.denis@gmail.com>
2019-03-23 23:43:56 +0300

    Add task25

---
+++ lesson_6/task25/src/main/java/pages/LoginPage.java
@@ -0,0 +1,47 @@
+﻿package pages;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class LoginPage {
+
+	@FindBy(xpath = "//input[@id='mailbox:login']")
+	private WebElement loginField;
+
+	@FindBy(xpath = "//input[@id='mailbox:password']")
+	private WebElement passwordField;
+
+	@FindBy(xpath = "//label[@id='mailbox:submit']")
+	private WebElement loginButton;
+
+	@FindBy(xpath = "//a[@id='PH_logoutLink']")
+	private WebElement logoutLink;
+
+	public LoginPage(WebDriver driver) {
+		PageFactory.initElements(driver, this);
+	}
+
+	public void enterDataForLogin(String login, String password) {
+		enterText(loginField, login);
+		enterText(passwordField, password);
+	}
+
+	public void clickEnterButton() {
+		loginButton.click();
+	}
+
+	public boolean isLogoutLinkDisplayed(WebDriver driver) {
+		new WebDriverWait(driver, 5).until(ExpectedConditions.titleContains("Входящие"));
+		return logoutLink.isDisplayed();
+	}
+
+	private void enterText(WebElement element, String text) {
+		element.clear();
+		element.sendKeys(text);
+	}
+
+}
---
+++ lesson_6/task25/src/main/java/testData/TestData.java
@@ -0,0 +1,64 @@
+﻿package testData;
+
+import dbSQL.SQLRequests;
+
+public class TestData {
+
+	String login;
+	String password;
+	String topic;
+	String mailCopy;
+	String recipient;
+
+	public TestData(String id) {
+		this.login = request.getValueParam("login", id);
+		this.password = request.getValueParam("password", id);
+		this.topic = request.getValueParam("topic", id);
+		this.mailCopy = request.getValueParam("text", id);
+		this.recipient = request.getValueParam("recipient", id);
+
+	}
+
+	SQLRequests request = new SQLRequests();
+
+	public String getLogin() {
+		return login;
+	}
+
+	public void setLogin(String login) {
+		this.login = login;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getTopic() {
+		return topic;
+	}
+
+	public void setTopic(String topic) {
+		this.topic = topic;
+	}
+
+	public String getMailCopy() {
+		return mailCopy;
+	}
+
+	public void setMailCopy(String mailCopy) {
+		this.mailCopy = mailCopy;
+	}
+
+	public String getRecipient() {
+		return recipient;
+	}
+
+	public void setRecipient(String recipient) {
+		this.recipient = recipient;
+	}
+
+}
