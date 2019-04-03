b7a81a09b80a39987159b88ee90cc117a1accaed
Denis Khtey <khtej.denis@gmail.com>
2019-04-03 16:40:39 +0300

    add hot fix for webdriver

--- lesson_6/project/src/main/java/webdriverSingleton/WebDriverSingleton.java
+++ lesson_6/project/src/main/java/webdriverSingleton/WebDriverSingleton.java
@@ -1,20 +1,26 @@
 package webdriverSingleton;
 
+import org.openqa.selenium.PageLoadStrategy;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 public class WebDriverSingleton {
 
 	private static WebDriver driver;
+	private static ChromeOptions options;
 
 	private WebDriverSingleton() {
 	}
 
 	public static WebDriver getWebDriverInstance() {
 		if (null == driver) {
-			driver = new ChromeDriver();
+			options = new ChromeOptions();
+			options.setPageLoadStrategy(PageLoadStrategy.NORMAL);
+			driver = new ChromeDriver(options);
 		}
 		return driver;
+
 	}
 
 	public static void driverQuit() {
